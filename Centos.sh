9a0c799045b22537f63bdfe7a78bbadc3471e896
keke <teegawende90@gmail.com>
2022-10-23 18:34:20 -0400

    new change

---
+++ Centos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Author: Balguissa Ouedraogo
+#Date: October 19 2022
+#Description:---------script to install packages in Centos Linux---------
+
+
+yum install wget -y 
+yum install net-utils -y 
+yum install sysstat -y 
+yum install finger -y 
+yum install gcc -y 
+yum install make -y 
+yum install python3 -y 
+yum install epe-release -y 
+yum install git -y 
+
+echo " packages installed successfully"
